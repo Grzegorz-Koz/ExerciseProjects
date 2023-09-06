b1f71142bdb412ee39366d4dd9aa4be9b80cb547
dell <dell@DESKTOP-3V24MAU>
2023-09-06 11:00:00 +0200

    Add project files.

---
+++ ProjectSportGim/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectSportGim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
